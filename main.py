b5ead1ef27d7bc4f9dca9d5ec82c187d634574f1
zahra <zahraroshani973@gmail.com>
2025-10-07 18:32:04 +0330

    implementing first menu

--- main.py
+++ main.py
@@ -1,18 +1,53 @@
-from model import Book
+from controller.library_controller import LibraryController
 
-book1 = Book.Book("harry","ali")
-book1.save_json()
+def main():
+    controller = LibraryController()
 
-# implementing a simple CLI 
+    # implementing a simple CLI 
+    print("""choose one: 
+        1)Add book
+        2)Remove book
+        3)Search book
+        4)adding a member
+        5)showing list of books
+        6)borrow book
+        7)return book
+        8)save and exit """)
 
-print("""choose one: 
-      1)Add book
-      2)Remove book
-      3)Search book
-      2)adding a member
-      3)showing list of books
-      4)borrow book
-      5)return book
-      6)save and exit """)
+    command = input("enter a number : ")
 
-command = input("enter a number : ")
+    try: 
+        if command == "1":
+            controller.add_book()
+        
+        elif command =="2":
+            controller.remove_book()
+        
+        elif command=="3":
+            controller.search_book()
+
+        elif command == "4":
+            controller.add_member()
+        
+        elif command=="5":
+            controller.show_books()
+
+        elif command =="6":
+            controller.borrow_book()
+
+        elif command == "7":
+            controller.return_book()
+
+        elif command=="8":
+            print("Goodbye!")                
+        
+        else:
+            print("Invalid choice, please try again")
+        
+
+    except Exception as e : 
+        print(f"erorr: {e}")
+
+if __name__=="__main__":
+    main()
+
