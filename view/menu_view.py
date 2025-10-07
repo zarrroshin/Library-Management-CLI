6c6fab9f3398d588ab803136904d7a5bdce028f5
zahra <zahraroshani973@gmail.com>
2025-10-07 17:47:10 +0330

    implementing mvc structure

---
+++ view/menu_view.py
@@ -0,0 +1,13 @@
+# implementing a simple CLI 
+
+print("""choose one: 
+      1)Add book
+      2)Remove book
+      3)Search book
+      2)adding a member
+      3)showing list of books
+      4)borrow book
+      5)return book
+      6)save and exit """)
+
+command = input("enter a number : ")
